0fdfee871ba24eeb205329675bf815b3d142eb3a
Roman <davidovshag@gmail.com>
2026-03-26 14:53:53 +0200

    working project

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,16 +1,38 @@
 import './App.css'
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
+// Імпортуємо дії з нашого слайса
+import {increment, decrement, incrementByValue} from "./slices/counterSlice.ts";
 
 function App() {
-    const {value: counter} = useSelector(store => store.counter)
+    // Отримало доступ до Storage Redux
+    const {value: counter} = useSelector(store => store.counter);
 
-    console.log("Store counter: " + counter)
-  return (
-    <>
-      <h1>Привіт із України :)</h1>
-        <h2>Counter: {counter}</h2>
-    </>
-  )
+    // Створюємо dispatch для відправки дій у Store
+    const dispatch = useDispatch();
+
+    console.log("Store counter: " + counter);
+
+    return (
+        <>
+            <h1>Привіт із України :)</h1>
+            <h2>Counter: {counter}</h2>
+
+            <div className="card">
+                {/* Викликаємо екшени при натисканні */}
+                <button onClick={() => dispatch(increment())}>
+                    Збільшити (+)
+                </button>
+
+                <button onClick={() => dispatch(decrement())}>
+                    Зменшити (-)
+                </button>
+
+                <button onClick={() => dispatch(incrementByValue(100))}>
+                    Встановити 100
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
